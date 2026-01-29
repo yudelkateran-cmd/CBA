777c3a55ad71be2052a8a88fc2c5cd441ef44cd5
Yudelka Reyes <yudebolivar@hotmail.com>
2026-01-29 19:04:49 -0300

    Primer commit: inicializando proyecto CBA

---
+++ app.tsx
@@ -0,0 +1,23 @@
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { Services } from "./components/Services";
+import { About } from "./components/About";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
